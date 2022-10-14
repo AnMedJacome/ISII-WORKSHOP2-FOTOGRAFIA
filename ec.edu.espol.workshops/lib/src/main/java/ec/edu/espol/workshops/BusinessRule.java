d19d0d85e4673c70882963e4320b36bb2cba89c7
koalaspero <diedarte@espol.edu.ec>
2022-10-13 19:27:39 -0500

    Reglas

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/BusinessRule.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/BusinessRule.java
@@ -2,8 +2,22 @@ package ec.edu.espol.workshops;
 
 public class BusinessRule {
     
-    public int reglaHombreJoven(Customer customer){
-        return 0;
+    public void reglaHombreJovenSoltero(Customer customer){
+        if(customer.getSex().toUpperCase().equals("M") && !customer.isMarried() && (customer.getAge()<25)){
+            customer.setBase(customer.getBase()+1500);
+        }
         
+    }
+
+    public void reglaMujerCasada(Customer customer){
+        if(customer.getSex().toUpperCase().equals("F") || customer.isMarried()){
+            customer.setBase(customer.getBase()-200);
+        }
+    }
+
+    public void reglaEdad(Customer customer){
+        if(customer.getAge()>=45 && customer.getAge()<65){
+            customer.setBase(customer.getBase()-100);
+        }
     }
 }
