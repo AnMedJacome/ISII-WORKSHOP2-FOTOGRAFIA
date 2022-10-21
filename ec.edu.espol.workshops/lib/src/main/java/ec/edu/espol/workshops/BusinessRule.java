367254a66877b2da5823d1c5028ba4f16dc19b29
Andres J. Medina J. <anmejaco@espol.edu.ec>
2022-10-21 18:31:04 -0500

    Solving problems using SpotBugs report.

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/BusinessRule.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/BusinessRule.java
@@ -1,31 +1,29 @@
 package ec.edu.espol.workshops;
 
+
 public class BusinessRule {
 	
-	private BusinessRule() {}
+  private BusinessRule() {}
 
-    public static void checkSingleYoungManRule(Customer customer){
-        if(customer.getSex().toUpperCase().equals("M") && !customer.isMarried() && (customer.getAge()<25)){
+  public static void checkSingleYoungManRule(Customer customer){
+        if(StringTool.compareUpperCaseString(customer.getSex(), "M") && !customer.isMarried() && (customer.getAge()<25)){
             customer.setBase(customer.getBase()+1500);
-        }        
-    }
-
-    public static void checkMarriedWomanRule(Customer customer){
-        if(customer.getSex().toUpperCase().equals("F") || customer.isMarried()){
-            customer.setBase(customer.getBase()-200);
         }
     }
 
-    public static void checkAgeRule(Customer customer){
-        if(customer.getAge()>=45 && customer.getAge()<65){
-            customer.setBase(customer.getBase()-100);
-        }
+  public static void checkMarriedWomanRule(Customer customer){
+    if(StringTool.compareUpperCaseString(customer.getSex(), "F") || customer.isMarried()){
+      customer.setBase(customer.getBase()-200);
     }
+  }
+
+  public static void checkAgeRule(Customer customer){
+    if(customer.getAge()>=45 && customer.getAge()<65){
+      customer.setBase(customer.getBase()-100);
+    }
+  }
     
-    public static void checkPremium(Customer cliente){
-        if(cliente.getAge()>80 || !cliente.hasDrivingLicense()){
-            System.out.println("Cannot sell insurance to current customer.");
-            System.exit(-1);
-        }
-    }
+  public static boolean checkPremium(Customer cliente){
+    return !(cliente.getAge()>80 || !cliente.hasDrivingLicense());
+  }
 }
--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -15,7 +15,7 @@ public class CarInsurance {
 			do {
 				System.out.println("Enter your sex(M/F): ");
 				sex = sc.nextLine();
-			} while(!(sex.toUpperCase()).equals("M") && !(sex.toUpperCase()).equals("F"));
+			} while(!(StringTool.compareUpperCaseString(sex, "M")) && !(StringTool.compareUpperCaseString(sex, "F")));
 
 			do {
 				System.out.println("Are you married or not?(Y/N): ");
@@ -27,17 +27,20 @@ public class CarInsurance {
 			do {
 				System.out.println("Do you have a driver's license?(Y/N): ");
 				license = sc.nextLine();
-			}while(!license.toUpperCase().equals("Y") && !license.toUpperCase().equals("N"));
+			}while(!(StringTool.compareUpperCaseString(license, "Y")) && !(StringTool.compareUpperCaseString(license, "N")));
 
-			boolean hasLicense = license.toUpperCase().equals("Y");
+			boolean hasLicense = StringTool.compareUpperCaseString(license, "Y");
 			Customer client = new Customer( Integer.parseInt(age), sex, married, hasLicense );
 
 			BusinessRule.checkSingleYoungManRule(client);
 			BusinessRule.checkMarriedWomanRule(client);
 			BusinessRule.checkAgeRule(client);
-			BusinessRule.checkPremium(client);
+			if(BusinessRule.checkPremium(client)) {
+	            System.out.println(client.toString());
+			} else {
+                System.out.println("Cannot sell insurance to current customer.");
+			}
 
-			System.out.println(client.toString());
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
